c3e7d930f7a6772240f38b9da6e074ca8939300c
MahdiYousefiIRN <Mahdi901548#>
2024-12-17 14:40:42 +0330

    add project Windows form And Project WPF

---
+++ WpfApptest Buttom1/WpfApptest Buttom1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApptest_Buttom1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+           
+                fanc12();
+               
+          
+         
+          
+
+        }
+
+       
+
+        private void fanc12()
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"exseption {e.Message}");
+                throw;
+            }
+           
+        }
+
+    }
+}
